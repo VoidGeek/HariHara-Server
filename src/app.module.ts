c1627f5c29d9affa8004428232cf0c87f4627baa
PradyLinux <pradyumna.p.6969@gmail.com>
2024-10-12 01:45:52 +0530

    Module Encapsulation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { SupabaseService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { StorageModule } from './storage/storage.module';
 
 @Module({
   imports: [AuthModule, StorageModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, SupabaseService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { SupabaseService } from './auth.service';
 
-@Module({})
+@Module({
+  providers: [SupabaseService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
