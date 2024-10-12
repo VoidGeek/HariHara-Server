f830dccab1385fa8097107ca58ff3b2351436ee2
PradyLinux <pradyumna.p.6969@gmail.com>
2024-10-13 00:07:34 +0530

    Lowered BoilerPlate

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { BaseService } from 'src/common/base,service';
+import { BaseService } from 'src/common/base.service';
 
 @Injectable()
 export class ContactsService extends BaseService {
